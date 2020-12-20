2465abc071d6087958d58e7db6d4414414a0570a
Jamal Mulla <jamaldevacc@gmail.com>
2020-12-20 15:56:30 +0000

    Initial

---
+++ cmake-build-debug/CMakeFiles/Simulator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Simulator.dir/main.cpp.o"
+  "Simulator"
+  "Simulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
