1e7ae3a8e38b55218b676b693ff0db9451fed41c
JamalMulla <jamaldevacc@gmail.com>
2021-02-22 23:24:50 +0000

    Still trying to get argument passing to work

--- architectures/main.cpp
+++ architectures/main.cpp
@@ -10,18 +10,20 @@
 #define END_TIMER() auto TIME_END = std::chrono::high_resolution_clock::now(); std::cout << "Elapsed time: " \
 << std::chrono::duration_cast<std::chrono::milliseconds>(TIME_END-TIME_START).count() << " ms\n"
 
-int main() {
+int main(int argc, char** argv) {
+
+    if (argc == 1) {
+        std::cerr << "[Error] No program file given" << std::endl;
+        exit(1);
+    }
+
     SCAMP5 s;
 
     s.scamp5_in(s.E, 10);
     int i = 0;
 
-
-
-
-    std::string test = R"(
-        get_image(C, D)
-    )";
+    InstructionParser<SCAMP5> parser(s.registers);
+    parser.parse(s, std::string(argv[1]), 0);
 
     // Assume we've parsed this and got the following
 //    const char *func_name = "get_image";
@@ -35,17 +37,17 @@ int main() {
 //    InstructionFactory<SCAMP5>::execute(s, func_name, a1, a2);
 //
 //    // Need to get the function using the type arguments. So need type arguments first
-    AREG *C;
-    AREG *D;
-    try
-    {
-        C = std::any_cast<AREG *>(s.registers.get_arg("C"));
-        D = std::any_cast<AREG *>(s.registers.get_arg("D"));
-    }
-    catch (const std::bad_any_cast& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+//    AREG* C;
+//    AREG* D;
+//    try
+//    {
+//        C = std::any_cast<AREG*>(s.registers.get_arg("C"));
+//        D = std::any_cast<AREG*>(s.registers.get_arg("D"));
+//    }
+//    catch (const std::bad_any_cast& e)
+//    {
+//        std::cout << e.what() << '\n';
+//    }
 
 
 //
@@ -53,33 +55,33 @@ int main() {
 //    // Get function
 //
 //    auto get_image = InstructionFactory<SCAMP5>::get_instruction<AREG, AREG>("get_image");
-
-    while(i < 250) {
-        START_TIMER();
-//        (s.*get_image)(s.C, s.D);
-        InstructionFactory<SCAMP5>::execute(s, "get_image", *C, *D);
-        END_TIMER();
-        s.get_image(s.C, s.D);
-
-        s.sub(s.A, s.C, s.E);
-        s.where(s.A);
-        s.MOV(s.R5, s.FLAG);
-        s.all();
-
-        utility::display_register("PIX", s.PIX);
-        utility::display_register("A", s.A);
-        utility::display_register("B", s.B);
-        utility::display_register("C", s.C);
-        utility::display_register("D", s.D);
-        utility::display_register("E", s.E);
-        utility::display_register("FLAG", s.FLAG);
-        utility::display_register("R5", s.R5);
-        utility::display_register("NEWS", s.NEWS);
-        cv::waitKey(1);
-        i++;
-        std::cout << "\rIteration " << i << std::endl;
-
-    }
+//
+//    while(i < 250) {
+//        START_TIMER();
+////        (s.*get_image)(s.C, s.D);
+//        InstructionFactory<SCAMP5>::execute(s, "get_image", *C, *D);
+//        END_TIMER();
+//        s.get_image(s.C, s.D);
+//
+//        s.sub(s.A, s.C, s.E);
+//        s.where(s.A);
+//        s.MOV(s.R5, s.FLAG);
+//        s.all();
+//
+//        utility::display_register("PIX", s.PIX);
+//        utility::display_register("A", s.A);
+//        utility::display_register("B", s.B);
+//        utility::display_register("C", s.C);
+//        utility::display_register("D", s.D);
+//        utility::display_register("E", s.E);
+//        utility::display_register("FLAG", s.FLAG);
+//        utility::display_register("R5", s.R5);
+//        utility::display_register("NEWS", s.NEWS);
+//        cv::waitKey(1);
+//        i++;
+//        std::cout << "\rIteration " << i << std::endl;
+//
+//    }
 
 //    std::unordered_map<std::string, void (SCAMP5::*)()> instructions;
 //    std::unordered_map<std::string, AnyCallable<void>()> instructions;
--- core/include/simulator/base/instruction_factory.h
+++ core/include/simulator/base/instruction_factory.h
@@ -9,10 +9,17 @@
 #include <unordered_map>
 #include <map>
 #include <typeinfo>
+#include <typeindex>
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <variant>
 #include <any>
+#include <algorithm>
+#include <functional>
+#include <simulator/registers/analogue_register.h>
+#include <simulator/registers/digital_register.h>
+
 
 template <class T, class... Args>
 struct InstructionHolder{
@@ -28,19 +35,70 @@ struct RegisterHolder{
 };
 
 
+//template <class T>
+//class InstructionFactory {
+//
+//public:
+////    InstructionHolder<T>* i;
+//
+//    InstructionFactory() {
+////        i = new InstructionHolder<T>();
+//    }
+//
+//    template<class...Args>
+//    void register_instruction(const std::string& name, void(T::*function)(Args&...)) {
+//        std::vector<std::type_index> vec;
+//        vec.insert(vec.end(), {typeid(Args)...});
+//        for (auto i: vec)
+//            std::cout << i.name() << ' ';
+//
+//        std::cout << "Instruction " << name << " registered " << std::endl;
+//        InstructionHolder<T, Args&...>::instructionMap[name] = function;
+//    }
+//
+//    template<class...Args>
+//    void execute(T& s, const std::string& name, Args&&... args) {
+//        std::vector<std::type_index> vec;
+//        vec.insert(vec.end(), {typeid(Args)...});
+//        for (auto i: vec)
+//            std::cout << i.name() << ' ';
+//
+//        if (InstructionHolder<T, Args...>::instructionMap.find(name) == InstructionHolder<T, Args...>::instructionMap.end()) {
+//            std::cerr << "Instruction " << name << " not found" << std::endl;
+//            exit(1);
+//        }
+//        (s.*InstructionHolder<T, Args&...>::instructionMap[name])(std::forward<Args>(args)...);
+//    }
+//
+//
+//
+//};
+
 template <class T>
 class InstructionFactory {
 
 public:
+
     template <class... Args>
-    static void register_instruction(std::string name, void(T::*function)(Args&...)) {
+    static void register_instruction(const std::string& name, void(T::*function)(Args&...)) {
         std::cout << "Instruction " << name << " registered " << std::endl;
+        std::vector<std::type_index> vec;
+        vec.insert(vec.end(), {typeid(Args)...});
+        for (auto i: vec)
+            std::cout << i.name() << ' ';
+        std::cout << std::endl;
         InstructionHolder<T, Args&...>::instructionMap[name] = function;
     }
 
     template <class... Args>
-    static auto get_instruction(std::string name) {
+    static auto get_instruction(const std::string& name) {
         #ifndef NDEBUG
+        std::vector<std::type_index> vec;
+        vec.insert(vec.end(), {typeid(Args)...});
+        for (auto i: vec)
+            std::cout << i.name() << ' ';
+        std::cout << std::endl;
+
         if (InstructionHolder<T, Args&...>::instructionMap.find(name) == InstructionHolder<T, Args&...>::instructionMap.end()) {
             std::cerr << "Instruction " << name << " not found" << std::endl;
             exit(1);
@@ -50,8 +108,14 @@ public:
     }
 
     template <class... Args>
-    static void execute(T& s, std::string name, Args &&... args) {
+    static void execute(T& s, const std::string& name, Args &&... args) {
         #ifndef NDEBUG
+        std::vector<std::type_index> vec;
+        vec.insert(vec.end(), {typeid(Args)...});
+        for (auto i: vec)
+            std::cout << i.name() << ' ';
+        std::cout << std::endl;
+
         if (InstructionHolder<T, Args&...>::instructionMap.find(name) == InstructionHolder<T, Args&...>::instructionMap.end()) {
             std::cerr << "Instruction " << name << " not found" << std::endl;
             exit(1);
@@ -76,7 +140,7 @@ public:
         std::cout << "Added argument " << name  << std::endl;
     }
 
-    [[nodiscard]] std::any& get_arg(const std::string& name) const {
+    [[nodiscard]] std::any get_arg(const std::string& name) const {
         #ifndef NDEBUG
         if (r->registerMap.find(name) == r->registerMap.end()) {
             std::cerr << "Argument " << name <<  " not found" << std::endl;
@@ -92,40 +156,156 @@ public:
 
 };
 
-//
-//class InstructionParser {
-//
-//    void parse_and_execute(std::string& program_file, int iterations);
-//
-//};
-//
-//void InstructionParser::parse_and_execute(std::string& program_file, int iterations) {
-//
-//    std::ifstream read(program_file);
-//    std::vector<std::pair<std::string, std::vector<std::string>>> instructionArgs;
-//
-//    // read until you reach the end of the file
-//    for (std::string line; std::getline(read, line); ) {
-//
-//        // removing punctuation like paranthesis, commas, etc.
-//        std::replace_if(std::begin(line), std::end(line), [] (char x) { return std::ispunct(x); }, ' ');
-//
-//        // inserting the line into a stream that helps us parse the content
-//        std::stringstream ss(line);
-//
-//        // read the node number
-//        std::string instr;
-//        ss >> instr;
-//
-//        // read the args until end of line
-//        std::vector<std::string> args;
-//        std::string arg;
-//        while (ss >> arg) {
-//            args.emplace_back(arg);
-//        }
-////        instructionArgs.emplace_back(std::make_pair<std::string, std::vector<std::string>>(instr, args));
-//    }
-//}
+template <class T>
+class InstructionParser {
+
+private:
+    ArgumentFactory& args_;
+public:
+
+    explicit InstructionParser(ArgumentFactory& args) :  args_(args) { }
+
+    void parse(T& s, const std::string& program_file, int iterations);
+    std::any get_arg(const std::string& name);
+    std::variant<AnalogueRegister*, DigitalRegister*> cast_arg(std::any& arg);
+
+};
+
+using namespace std::placeholders;  // for _1, _2, _3...
+
+
+template<class T>
+void InstructionParser<T>::parse(T& s, const std::string& program_file, int iterations) {
+
+    std::ifstream file(program_file);
+
+    if(!file.is_open()) {
+        std::cerr << "Error opening file " << program_file << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::vector<std::pair<std::string, std::vector<std::string>>> instructionArgs;
+
+    // read until you reach the end of the file
+
+    std::string line;
+    while (std::getline(file, line)) {
+
+        // inserting the line into a stream that helps us parse the content
+
+        unsigned first = line.find('(');
+        unsigned last = line.find(')');
+        std::string arg_string = line.substr (first+1, last-first-1);
+
+        //replace commas with space
+        std::replace(arg_string.begin(), arg_string.end(), ',', ' ');
+        std::stringstream arg_stream(arg_string);
+
+        std::replace(line.begin(), line.end(), '(', ' ');
+        std::replace(line.begin(), line.end(), ')', ' ');
+
+        // read the node number
+        std::stringstream ss(line);
+        std::string instr;
+        ss >> instr;
+
+        std::cout << "Executing instruction " << instr << std::endl;
+
+        // read the args until end of line
+        std::vector<std::any> args;
+        std::string arg;
+        while (arg_stream >> arg) {
+            std::cout << "argument: " << arg << std::endl;
+            args.emplace_back(get_arg(arg));
+        }
+
+        switch (args.size()) {
+            case 0: {
+                InstructionFactory<T>::execute(s, instr);
+                break;
+            }
+            case 1: {
+                auto f1 = std::bind(&InstructionFactory<T>::template execute<AnalogueRegister>, s, instr, _1);
+
+                auto arg1 = args.at(0);
+
+                if (arg1.type() == typeid(AnalogueRegister*)) {
+                    f1 = std::bind(f1, *(std::any_cast<AnalogueRegister*>(arg1)));
+                } else if (arg1.type() == typeid(DigitalRegister*)) {
+                    f1 = std::bind(f1, *(std::any_cast<DigitalRegister*>(arg1)));
+                } else {
+                    std::cerr << "Unable to cast arg of type " << arg1.type().name() << std::endl;
+                    exit(1);
+                }
+                
+                f1();
+                break;
+            }
+            case 2: {
+                auto f1 = std::bind(&InstructionFactory<T>::template execute<AnalogueRegister, AnalogueRegister>, s, instr, _1);
+
+                auto arg1 = args.at(0);
+                auto arg2 = args.at(1);
+
+                if (arg1.type() == typeid(AnalogueRegister*)) {
+                    f1 = std::bind(f1, *(std::any_cast<AnalogueRegister*>(arg1)), _1);
+                } else if (arg1.type() == typeid(DigitalRegister*)) {
+                    f1 = std::bind(f1, *(std::any_cast<DigitalRegister*>(arg1)), _1);
+                } else {
+                    std::cerr << "Unable to cast arg of type " << arg1.type().name() << std::endl;
+                    exit(1);
+                }
+
+                if (arg2.type() == typeid(AnalogueRegister*)) {
+                    f1 = std::bind(f1, *(std::any_cast<AnalogueRegister*>(arg2)));
+                } else if (arg2.type() == typeid(DigitalRegister*)) {
+                    f1 = std::bind(f1, *(std::any_cast<DigitalRegister*>(arg2)));
+                } else {
+                    std::cerr << "Unable to cast arg of type " << arg2.type().name() << std::endl;
+                    exit(1);
+                }
+
+                f1();
+                break;
+            }
+            default: {
+                std::cerr << "Cannot support more than 2 arguments at this time" << std::endl;
+                exit(1);
+            }
+        }
+
+
+
+//        instructionArgs.emplace_back(std::make_pair<std::string, std::vector<std::string>>(instr, args));
+
+
+    }
+
+}
+
+constexpr unsigned int hash(const char *s, int off = 0) {
+    return !s[off] ? 5381 : (hash(s, off+1)*33) ^ s[off];
+}
+
+constexpr inline unsigned int operator "" _(char const * p, size_t) { return hash(p); }
+
+template<class T>
+std::any InstructionParser<T>::get_arg(const std::string& name) {
+    return this->args_.get_arg(name);
+}
+
+template<class T>
+std::variant<AnalogueRegister*, DigitalRegister*> InstructionParser<T>::cast_arg(std::any& arg) {
+    if (arg.type() == typeid(AnalogueRegister*)) {
+        return std::any_cast<AnalogueRegister*>(arg);
+    } else if (arg.type() == typeid(DigitalRegister*)) {
+        return std::any_cast<DigitalRegister*>(arg);
+    } else {
+        std::cerr << "Unable to cast arg of type " << arg.type().name() << std::endl;
+        exit(1);
+    }
+
+}
 
 
 #endif //SIMULATOR_INSTRUCTION_FACTORY_H
