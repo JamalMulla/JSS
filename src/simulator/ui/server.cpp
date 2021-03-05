f5492bce83e471eaf6f37ae444f46f88abe45c19
Jamal Mulla <jamaldevacc@gmail.com>
2021-03-05 12:42:19 +0000

    Added filewatcher auto refresh of served files

--- src/simulator/ui/server.cpp
+++ src/simulator/ui/server.cpp
@@ -7,10 +7,11 @@
 #include <iostream>
 #include "simulator/ui/server.h"
 #include "simulator/ui/async_file_streamer.h"
+#include "simulator/ui/file_watcher.h"
 #include <thread>
 
 
-std::string file_path(const char* path) {
+std::string file_path(const char *path) {
     std::string path_s = path;
     return path_s.substr(0, path_s.find_last_of("\\/"));
 }
@@ -19,7 +20,20 @@ void Server::server_run() {
     std::string serving_dir = file_path(__FILE__) + std::filesystem::path::preferred_separator + "src";
 
     AsyncFileStreamer asyncFileStreamer(serving_dir);
-    //todo refactor into class
+    FileWatcher fw{serving_dir, std::chrono::milliseconds(3000)};
+
+    std::thread filewatcher_t([&fw, &asyncFileStreamer] {
+        fw.start([&asyncFileStreamer](std::string path_to_watch, FileStatus status) -> void {
+            // Process only regular files, all other file types are ignored
+            if (!std::filesystem::is_regular_file(std::filesystem::path(path_to_watch)) && status != FileStatus::erased) {
+                return;
+            }
+            std::cout << "Updating files" << std::endl;
+            asyncFileStreamer.updateRootCache();
+        });
+    });
+    filewatcher_t.detach();
+
 
     uWS::App().get("/*", [&, &asyncFileStreamer](auto *res, auto *req) {
 
@@ -50,12 +64,12 @@ void Server::server_run() {
 }
 
 void Server::start() {
-    std::thread server_t (&Server::server_run, this);
+    std::thread server_t(&Server::server_run, this);
     server_t.detach();
 }
 
 void Server::send_string(const std::string &data) const {
-    for (auto& ws : wss) {
+    for (auto &ws : wss) {
         ws->send(data, uWS::OpCode::TEXT);
     }
 }
