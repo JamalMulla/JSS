1a28fee809fca01538ade9e3070ae2b7da1076f5
Jamal Mulla <jamaldevacc@gmail.com>
2021-03-13 21:20:30 +0000

    Cleaning up

--- architectures/main.cpp
+++ architectures/main.cpp
@@ -2,16 +2,6 @@
 // Created by Jamal on 20/02/2021.
 //
 
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "EndlessLoop"
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
-
-#include "analognet2/analog_main.h"
-
-
 int main() {
-    analog_main();
     return 0;
 }
-
-#pragma clang diagnostic pop
