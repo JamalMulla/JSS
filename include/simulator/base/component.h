1b11b80824fed53fe86a93293b26d33e87cdb8cb
Jamal Mulla <jamaldevacc@gmail.com>
2021-04-08 23:54:50 +0100

    starting power/area/time components

--- include/simulator/base/component.h
+++ include/simulator/base/component.h
@@ -13,6 +13,18 @@ using json = nlohmann::json;
 class Component {
    public:
 #ifdef TRACK_STATISTICS
+    /* Returns the amount of static power dissipation in the amount of time passed.*/
+    virtual double get_static_power(double time) = 0;
+    /* Returns the amount of dynamic power dissipation.
+     * The operation is executed first and the amount of dynamic power is stored.
+     * This is then returned when this method is called. */
+    virtual double get_dynamic_power() = 0;
+    /* Simply the number of transistors needed for the component.
+     * This should be a constant after the component is configured. */
+    virtual int get_transistor_count() = 0;
+    /* The number of cycles needed for the operation.
+     * Similar to dynamic power as the value is stored after an operation*/
+    virtual int get_cycle_count() = 0;
     virtual void print_stats(const CycleCounter& counter) = 0;
     virtual void write_stats(const CycleCounter& counter, json& j) = 0;
 #endif
