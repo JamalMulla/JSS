9dd82aef3147749f7887c33dab702eec720ba66f
Jamal Mulla <jamaldevacc@gmail.com>
2021-04-25 22:57:12 +0100

    Improved parser and registered many more instructions

--- scamp5/main.cpp
+++ scamp5/main.cpp
@@ -52,9 +52,8 @@ int main() {
     while(i < frames) {
         int e1 = cv::getTickCount();
         rttr_test(s);
-//        motion_detect(s);
         int e2 = cv::getTickCount();
-//        multiple_sobel(s);
+//        multiple_sobel.txt(s);
 //        s.histogram(s.A);
         std::cout << ((e2 - e1) / cv::getTickFrequency()) * 1000 << " ms" << std::endl;
         ui.display_reg(s.A);
@@ -80,82 +79,32 @@ using namespace rttr;
 
 inline void rttr_test(SCAMP5& s) {
 
-    static bool has_parsed = false;
-    const std::string program = "/home/jm1417/Simulator/misc/sobel.txt";
+    const std::string program = "/home/jm1417/Simulator/scamp5/examples/multiple_sobel.txt";
 
     type scamp = type::get_by_name("SCAMP5");
     std::vector<std::pair<rttr::method, std::vector<rttr::variant> > > parsed = InstructionParser::parse(scamp, rttr::instance(s), program);
-    has_parsed = true;
 
 
     for(auto& instr: parsed) {
         method& method = instr.first;
         std::vector<rttr::variant>& args = instr.second;
+        variant res;
         switch(args.size()) {
-            case 0: method.invoke(s); break;
-            case 1: method.invoke(s, args[0]); break;
-            case 2: method.invoke(s, args[0], args[1]); break;
-            case 3: method.invoke(s, args[0], args[1], args[2]); break;
-            case 4: method.invoke(s, args[0], args[1], args[2], args[3]); break;
-            case 5: method.invoke(s, args[0], args[1], args[2], args[3], args[4]); break;
+            case 0: res = method.invoke(s); break;
+            case 1: res = method.invoke(s, args[0]); break;
+            case 2: res = method.invoke(s, args[0], args[1]); break;
+            case 3: res = method.invoke(s, args[0], args[1], args[2]); break;
+            case 4: res = method.invoke(s, args[0], args[1], args[2], args[3]); break;
+            case 5: res = method.invoke(s, args[0], args[1], args[2], args[3], args[4]); break;
             default: std::cerr << "Too many arguments in method " << method.get_name() << " invocation" << std::endl;
         }
+        if (!res.is_valid()) {
+            std::cerr << "Could not execute method \"" << method.get_name() << "\"" << " with " << args.size() << " arguments" << std::endl;
+            exit(EXIT_FAILURE);
+        }
     }
-
-
-//    type class_type = type::get_by_name("SCAMP5");
-//    property A = class_type.get_property("A");
-//    property B = class_type.get_property("B");
-//    property D = class_type.get_property("D");
-//    method get_image = class_type.get_method("get_image");
-//    method movx = class_type.get_method("movx");
-//    method addx = class_type.get_method("addx");
-//    method sub2x = class_type.get_method("sub2x");
-//
-//    get_image.invoke(s, A.get_value(s), D.get_value(s));
-//    movx.invoke(s, B.get_value(s), A.get_value(s), south);
-//    addx.invoke(s, A.get_value(s), B.get_value(s), A.get_value(s), north);
-//    addx.invoke(s, B.get_value(s), B.get_value(s), A.get_value(s), east);
-//    sub2x.invoke(s, A.get_value(s), B.get_value(s), west, west, B.get_value(s));
 }
 
-inline void vertical_sobel(SCAMP5& s) {
-    s.get_image(s.A, s.D);
-//    s.movx(s.B, s.A, south);
-//    s.add(s.B, s.B, s.A);
-//    s.movx(s.A, s.B, north);
-//    s.addx(s.B, s.B, s.A, east);
-//    s.sub2x(s.A, s.B, west, west, s.B);
-    s.movx(s.B, s.A, south);
-    s.addx(s.A, s.B, s.A, north);
-    s.addx(s.B, s.B, s.A, east);
-    s.sub2x(s.A, s.B, west, west, s.B);
-}
-
-inline void horizontal_sobel(SCAMP5& s) {
-    s.get_image(s.A, s.D);
-//    s.movx(s.B, s.A, west);
-//    s.addx(s.A, s.B, s.A, east);
-//    s.addx(s.B, s.B, s.A, south);
-//    s.sub2x(s.A, s.B, north, north, s.B);
-    s.movx(s.B, s.A, south);
-    s.mov2x(s.D, s.A, west, north);
-    s.sub(s.C, s.A, s.B);
-    s.add(s.A, s.A, s.C);
-    s.movx(s.B, s.A, west);
-    s.sub2x(s.C, s.C, north, east, s.D);
-    s.add(s.A, s.B, s.A, s.C);
-}
-
-inline void superpixel_sobel(SCAMP5& s) {
-    s.get_image(s.A, s.D);
-    s.superpixel_adc(s.R5, 0, s.A);
-    s.superpixel_movx(s.R6, s.R5, south);
-    s.superpixel_movx(s.R7, s.R6, north);
-    s.superpixel_movx(s.R7, s.R7, north);
-    s.superpixel_sub(s.R5, 0, s.R7, s.R6);
-    s.superpixel_dac(s.B, 0, s.R5);
-}
 
 inline void superpixel(SCAMP5& s) {
     s.get_image(s.A, s.D);
@@ -171,71 +120,6 @@ inline void superpixel(SCAMP5& s) {
 //    s.superpixel_dac(s.C, 1, s.R5);
 }
 
-inline void multiple_sobel(SCAMP5& s) {
-    s.get_image(s.A, s.D);
-    s.neg(s.C, s.A);
-    s.subx(s.B, s.A, south, s.C);
-    s.subx(s.D, s.A, north, s.C);
-    s.subx(s.C, s.C, west, s.A);
-    s.movx(s.A, s.C, east);
-    s.addx(s.C, s.C, s.A, north);
-    s.addx(s.B, s.B, s.D, east);
-    s.sub2x(s.A, s.B, west, west, s.B);
-    s.sub2x(s.B, s.C, south, south, s.C);
-}
 
-inline void gaussian3x3(SCAMP5& s) {
-    s.get_image(s.A, s.D);
-    s.divq(s.C, s.A);
-    s.divq(s.A, s.C);
-    s.divq(s.C, s.A);
-    s.divq(s.A, s.C);
-    s.movx(s.C, s.A, south);
-    s.add(s.A, s.C, s.A);
-    s.movx(s.C, s.A, north);
-    s.add(s.A, s.A, s.C);
-    s.movx(s.C, s.A, west);
-    s.add(s.A, s.C, s.A);
-    s.movx(s.C, s.A, east);
-    s.add(s.A, s.A, s.C);
-}
-
-inline void gaussian5x5(SCAMP5& s) {
-    s.get_image(s.A, s.D);
-    s.div(s.E, s.C, s.A);
-    s.diva(s.E, s.C, s.B);
-    s.diva(s.E, s.C, s.B);
-    s.diva(s.E, s.C, s.B);
-    s.diva(s.E, s.C, s.B);
-    s.neg(s.C, s.E);
-    s.neg(s.B, s.C);
-    s.diva(s.B, s.C, s.F);
-    s.movx(s.C, s.B, south);
-    s.movx(s.F, s.B, north);
-    s.add(s.D, s.B, s.E);
-    s.add(s.B, s.B, s.E, s.D);
-    s.addx(s.D, s.E, s.D, north);
-    s.add(s.C, s.C, s.E, s.F);
-    s.mov2x(s.A, s.C, west, north);
-    s.add2x(s.F, s.E, s.F, north, north);
-    s.add2x(s.D, s.E, s.D, south, south);
-    s.add2x(s.E, s.D, s.E, north, north);
-    s.add(s.A, s.F, s.C, s.A);
-    s.add2x(s.A, s.A, s.E, west, south);
-    s.add(s.B, s.B, s.D, s.E);
-    s.add2x(s.C, s.C, s.A, east, east);
-    s.add(s.A, s.B, s.A, s.C);
-}
-
-inline void motion_detect(SCAMP5& s) {
-    s.get_image(s.C,s.D);
-    s.sub(s.D,s.C,s.F);	// D = C - F
-    s.mov(s.F,s.C);	// F = C
-    s.abs(s.B,s.D);	// B = |D|
-    s.sub(s.A,s.B,s.E);	// A = B - E
-    s.where(s.A);	// where A > 0
-        s.MOV(s.R5, s.FLAG);
-    s.all();
-}
 
 #pragma clang diagnostic pop
--- src/simulator/external/instruction_parser.cpp
+++ src/simulator/external/instruction_parser.cpp
@@ -11,7 +11,7 @@
 #include <sstream>
 #include <vector>
 
-#include "../../../../.conan/data/rttr/0.9.6/_/_/package/b173bbda18164d49a449ffadc1c9e817f49e819d/include/rttr/enumeration.h"
+#include <rttr/enumeration.h>
 
 std::vector<std::pair<rttr::method, std::vector<rttr::variant> > > InstructionParser::parse(const rttr::type& class_type, rttr::instance obj, const std::string& program_file) {
     std::ifstream file(program_file);
@@ -27,6 +27,9 @@ std::vector<std::pair<rttr::method, std::vector<rttr::variant> > > InstructionPa
 
     std::string line;
     while(std::getline(file, line)) {
+        if (line.empty()) {
+            continue;
+        }
         unsigned first = line.find('(');
         unsigned last = line.find(')');
         std::string arg_string = line.substr(first + 1, last - first - 1);
@@ -45,13 +48,14 @@ std::vector<std::pair<rttr::method, std::vector<rttr::variant> > > InstructionPa
 
         // read the args until end of line
         std::vector<rttr::variant> args;
+        std::vector<rttr::type> arg_types; // used for selecting the right overloaded method
         std::string arg;
 
-        // todo add some flexiblity here. maybe a list of types to check
+        // todo add some flexiblity here. maybe a list of types to check?
         rttr::type enum_type = rttr::type::get_by_name("news_t");
         if (!enum_type || !enum_type.is_enumeration())
         {
-            std::cout << "No enum for news_t found" << std::endl;
+            std::cerr << "No enum for news_t found" << std::endl;
         }
         rttr::enumeration enum_news = enum_type.get_enumeration();
 
@@ -62,10 +66,21 @@ std::vector<std::pair<rttr::method, std::vector<rttr::variant> > > InstructionPa
                 arg_val = enum_news.name_to_value(arg);
             }
 
+            if (!arg_val.is_valid()) {
+                std::cerr << "Argument " << arg << " is invalid" << std::endl;
+            }
+
             args.emplace_back(arg_val);
+            arg_types.push_back(arg_val.get_type());
         }
 
-        instructionArgs.emplace_back(class_type.get_method(instr), args);
+        rttr::method instr_method = class_type.get_method(instr, arg_types);
+
+        if (!instr_method.is_valid()) {
+            std::cerr << "Method \"" << instr << "\" not found. It may not have been registered." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        instructionArgs.emplace_back(instr_method, args);
     }
     return instructionArgs;
 }
